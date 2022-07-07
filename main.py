8e13c1300445fb1112ec681dacbd9dc4e9639b9e
zchronis <60366837+zchronis@users.noreply.github.com>
2022-07-07 14:56:44 -0700

    Creating sftp connection
    able to connect to linux.cs.pdx.edu using pysftp, used getpass lib to keep password secure

---
+++ main.py
@@ -0,0 +1,8 @@
+import pysftp #Make sure to run "pip install pysftp" in terminal
+from getpass import getpass #for making password input protected
+
+address = input("Enter server address: ")
+username = input("Username: ")
+password = getpass("Password: ")
+
+sftp = pysftp.Connection(address, username=username, password=password)
