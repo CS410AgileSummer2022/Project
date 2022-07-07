03d88440bd9095e61e9c55a2425f288870772539
Bishop <59637502+Iwasson@users.noreply.github.com>
2022-07-07 15:12:30 -0700

    Adding mkdir
    Enabled mkdir functionality. Currently to test it we call the function and output a print statement

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import pysftp #Make sure to run "pip install pysftp" in terminal
 from getpass import getpass #for making password input protected
 
+def makeDir(stfp, dirName):
+    sftp.mkdir(dirName)
+    print(dirName + " has been made!")
+
+
 address = input("Enter server address: ")
 username = input("Username: ")
 password = getpass("Password: ")
 
 sftp = pysftp.Connection(address, username=username, password=password)
+dirName = input("Please enter a directory name: ")
+makeDir(sftp, dirName)
 
 sftp.close()
