28b557659dc2f9a9285f8da65b8168fd89879caf
Ben Williams <bewi2@pdx.edu>
2022-07-09 15:07:37 -0700

    Wrapped driver code into main function. Added functions to list contents of directories on local machine and remote host. Added work around for bug when connection to remote host. Set a cwd after connecting to remote host.

--- main.py
+++ main.py
@@ -1,17 +1,62 @@
 import pysftp #Make sure to run "pip install pysftp" in terminal
 from getpass import getpass #for making password input protected
+import os
 
-def makeDir(stfp, dirName):
+
+class Local:
+    def printLocalDirectory(self, path):
+        dir_list = os.listdir(path)
+        for item in dir_list:
+            print(item)
+
+
+def makeDir(sftp, dirName):
     sftp.mkdir(dirName)
     print(dirName + " has been made!")
 
 
-address = input("Enter server address: ")
-username = input("Username: ")
-password = getpass("Password: ")
+# Print the Current Working Directory in the remote host
+def printRemoteWorkingDirectory(sftp):
+    workingDirectory = sftp.getcwd()
+    print(f"cwd: {workingDirectory}")
 
-sftp = pysftp.Connection(address, username=username, password=password)
-dirName = input("Please enter a directory name: ")
-makeDir(sftp, dirName)
 
-sftp.close()
+# Print the contents of a directory on the remote host from the specified path
+def printRemoteDirectory(sftp, path):
+    dirContents = sftp.listdir()
+    for item in dirContents:
+        print(item)
+
+
+def main():
+    local = Local()
+    localPath = input("Specify Local Path: ")
+    local.printLocalDirectory(localPath)
+
+    address = input("Enter server address: ")
+    username = input("Username: ")
+    password = getpass("Password: ")
+
+    # 'Fixes' known bug with pysftp where no hostkey is found for remote server. 
+    # See https://stackoverflow.com/questions/53864260/no-hostkey-for-host-found-when-connecting-to-sftp-server-with-pysftp-usi
+    cnopts = pysftp.CnOpts()
+    cnopts.hostkeys = None
+
+    sftp = pysftp.Connection(address, username=username, password=password, cnopts=cnopts)
+    
+    # Set a current working directory directly succeeding connection to remote host (works for linux.cs.pdx.edu)
+    sftp.cwd(f"/u/{username}")
+
+    printRemoteWorkingDirectory(sftp)
+
+    remotePath = input("Specify Remote Path: ")
+    printRemoteDirectory(sftp, remotePath)
+
+    dirName = input("Please enter a directory name: ")
+    makeDir(sftp, dirName)
+
+    sftp.close()
+
+
+if __name__ == "__main__":
+    main()
