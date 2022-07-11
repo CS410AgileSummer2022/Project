912eb7160535c542895f439df674fe14e8e141e3
Stoic_pdx <bkoehler2019@gmail.com>
2022-07-11 15:54:19 -0700

    Added new def Testing from main

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,18 @@
 import pytest
 #Looking into python mock library
 
-#Testing makeDir from main.
+#Testing make Directory from main.
 def test_makeDir():
     pass
 
+#Testing print Remote Working Directory
+def test_printRemoteWorkingDirectory():
+    pass
+
+#Testing Print Remote Directory
+def test_printRemoteDirectory():
+    pass
+
+#Testing Main
+def test_main():
+    pass
