b77dacb08e90820df1c4a565a9a4316faf930803
Naya <44099724+nayamairena@users.noreply.github.com>
2022-07-24 15:06:44 -0700

    Squashed commit of the following:
    commit 1b5cbda3dec18268744fdef775cb3fa1609251bf
    Merge: 962aa37 f62fcb3
    Author: Iwasson <59637502+Iwasson@users.noreply.github.com>
    Date:   Sun Jul 24 12:16:33 2022 -0700
    
        Merge pull request #22 from CS410AgileSummer2022/renameFileLocal
    
        Added function to rename local file
    
    commit f62fcb3998166a44570c082036a160840c33e33e
    Author: Ben Williams <bewi2@pdx.edu>
    Date:   Sun Jul 24 11:55:03 2022 -0700
    
        Added function to rename local file
    
    commit 962aa3769186e4878d88c451d0576a3f6fd1fc3d
    Merge: 9af4217 81c64e0
    Author: Iwasson <59637502+Iwasson@users.noreply.github.com>
    Date:   Thu Jul 21 17:05:11 2022 -0700
    
        Merge pull request #15 from CS410AgileSummer2022/unit_test
    
        Unit test
    
    commit 81c64e0c7ded0ffbe994d355da9e088362c76b5a
    Author: Stoic_pdx <bkoehler2019@gmail.com>
    Date:   Thu Jul 21 17:02:50 2022 -0700
    
        Update test_main.py
    
        Added unit_test.py file. Mock is still unstable for test. Set to pass
    
    commit 99b3660a319c7f7b7b58935a4608ef429a2c6bf3
    Merge: e982f66 9af4217
    Author: Stoic_pdx <bkoehler2019@gmail.com>
    Date:   Thu Jul 21 17:01:50 2022 -0700
    
        Merge branch 'main' into unit_test
    
    commit 9af42172c1336c7b3661e5041895ddbc46ea2f65
    Merge: 4dbd26f 3b555f4
    Author: zchronis <60366837+zchronis@users.noreply.github.com>
    Date:   Thu Jul 21 13:22:46 2022 -0700
    
        Merge pull request #20 from CS410AgileSummer2022/chmod
    
        Chmod
    
    commit 3b555f405be3d329c7cbb0326a3fee7fac88fa82
    Merge: 4e89c71 4dbd26f
    Author: Bishop <59637502+Iwasson@users.noreply.github.com>
    Date:   Thu Jul 21 13:21:12 2022 -0700
    
        Merge branch 'main' into chmod
    
    commit 4e89c71486a4ef1d1b5f4b425d41bc35d2113ddc
    Author: Bishop <59637502+Iwasson@users.noreply.github.com>
    Date:   Thu Jul 21 13:19:13 2022 -0700
    
        Update main.py
    
        Adding change permissions for remote files and dirs
    
    commit 4dbd26f958c064a764c7c00ad561cb2618b6f7ce
    Merge: 01d3969 e33e444
    Author: Iwasson <59637502+Iwasson@users.noreply.github.com>
    Date:   Thu Jul 21 13:18:37 2022 -0700
    
        Merge pull request #19 from CS410AgileSummer2022/getFile
    
        Update to getFile for multiple files
    
    commit e33e4444c1f710f311b2c3056e6e08b261959f5b
    Author: zchronis <60366837+zchronis@users.noreply.github.com>
    Date:   Thu Jul 21 13:11:57 2022 -0700
    
        Update to getFile for multiple files
    
    commit e982f66994d78a4e68f9de6842bbbd7f9cea0463
    Merge: 591575f 01d3969
    Author: Stoic_pdx <bkoehler2019@gmail.com>
    Date:   Sun Jul 17 16:19:52 2022 -0700
    
        Merge branch 'main' into unit_test
    
    commit 591575fd2407bdaca71ac9ca9320eb05eb041eaa
    Author: Stoic_pdx <bkoehler2019@gmail.com>
    Date:   Wed Jul 13 13:17:44 2022 -0700
    
        Testing Formatter
    
    commit 1c56a73d17046227951a3ea78a58edbdea59ec92
    Author: Stoic_pdx <bkoehler2019@gmail.com>
    Date:   Wed Jul 13 12:27:33 2022 -0700
    
        added gitignore vscode file
    
    commit 8b09fb0f72b2a5c242220734e9e6d3c8ff7fb868
    Author: Stoic_pdx <bkoehler2019@gmail.com>
    Date:   Wed Jul 13 12:23:43 2022 -0700
    
        pytest's added settings.json
    
    commit 912eb7160535c542895f439df674fe14e8e141e3
    Author: Stoic_pdx <bkoehler2019@gmail.com>
    Date:   Mon Jul 11 15:54:19 2022 -0700
    
        Added new def Testing from main
    
    commit ca5d7fb34b2a56ca67848bc23bcc06a20ec2d966
    Author: Stoic_pdx <bkoehler2019@gmail.com>
    Date:   Mon Jul 11 15:50:53 2022 -0700
    
        Added comments
    
    commit 99d6e21f025adde8782b2f685d5682c69b232d07
    Merge: 09c72f7 624c381
    Author: Koehler4 <koehler4@pdx.edu>
    Date:   Sun Jul 10 15:55:01 2022 -0700
    
        Merge branch 'main' into unit_test
    
    commit 09c72f7919be20dd2a1fafef71b5961373ca99f7
    Author: Stoic_pdx <bkoehler2019@gmail.com>
    Date:   Sun Jul 10 13:43:04 2022 -0700
    
        Testing pytest functionality
    
    commit a8d22c8461a900f68d8a602a88ea07433da72dd6
    Author: Stoic_pdx <bkoehler2019@gmail.com>
    Date:   Thu Jul 7 20:21:30 2022 -0700
    
        Added unittest folder and file. .gitignore

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
+from importlib.resources import path
 from types import NoneType
-import pysftp #Make sure to run "pip install pysftp" in terminal
-from getpass import getpass #for making password input protected
-import os
-#import tkinter as tk
-#from tkinter import *
-#from tkinter import tkk
+import pysftp                               # Make sure to run "pip install pysftp" in terminal
+from getpass import getpass                 # For making password input protected
+import os                                   # Utils for native OS
+from stat import S_ISDIR, S_ISREG           # Needed for making get_r portable
+import re                                   # Python Regex
 
+# Print contents of a directory on local machine at specified path
+def printLocalDirectory(path):
+    dir_list = os.listdir(path)
+    for item in dir_list:
+        print(item)
 
-class Local:
-    def printLocalDirectory(self, path):
-        dir_list = os.listdir(path)
-        for item in dir_list:
-            print(item)
-
+# Rename a file on local machine using full path to file and a new file name 
+def renameLocalFile(path_to_file):
+    old_file_name = os.path.basename(path_to_file)
+    new_file_name = input("New file name: ")
+    path_to_dir = os.path.dirname(path_to_file)
+    os.rename(f"{path_to_file}", f"{path_to_dir}\\{new_file_name}")
+    print(f"{old_file_name} has been successfully renamed to {new_file_name} in the local directory {path_to_dir}")
 
 def makeDir(sftp, dirName):
     sftp.mkdir(dirName)
     print(dirName + " has been made!")
 
-
 # Print the Current Working Directory in the remote host
 def printRemoteWorkingDirectory(sftp):
     workingDirectory = sftp.getcwd()
     print(f"cwd: {workingDirectory}")
-
 
 # Print the contents of a directory on the remote host from the specified path
 def printRemoteDirectory(sftp, path):
@@ -45,29 +49,79 @@ def login(address, username, password):
 # Get file from remote server
 def getFile(sftp, path, dest):
     dest = dest.replace('\\', '/')
+    #check if the destination exists
+    if(os.path.exists(dest) == False):
+        print("Destination path does not exist.")
+        return
+
+    #checks if the last character of the destination is a /
+    if(dest[-1] != "/"):
+        dest = dest+"/"
+
+    # if the path is a file, then only download one thing
     if(sftp.isfile(path)):
-        #checks if the last character of the destination is a \
-        if(dest[-1] != "/"):
-            dest = dest+"/"
-
-        #check if the destination exists
-        if(os.path.exists(dest) == False):
-            print("Destination path does not exist.")
-            return
-
         #appends the file name to the destination path
         dest = dest+path.split("/")[-1]
 
         #download the file
         sftp.get(path, dest)
         print("Success!")
+
+    # if the path is a dir, then copy the whole dir
+    elif(sftp.isdir(path)):
+        get_r_portable(sftp, path, dest)
+        print("Success!")
+
     else:
         print("Specified path is not a file.")
+ 
+# change the perms on the remote server
+def chmod(sftp, path):
+    # check if the remote path exists
+    if(sftp.lexists(path) == False):
+        print("Remote path does not exist!")
+        return
 
-def menuLoop(sftp, local):
+    reg = re.compile('[0-7]+')                  # regex to match for octal strings
+    mode = input("Please enter octal code: ")   # capture the user input
+    match = reg.match(mode)                     # compare the input string to the octal regex
+    
+    # ensures that the match is consitent across the whole string
+    if(match.start() == 0 and match.end() == len(mode)):
+        sftp.chmod(path, mode)
+        return
+    else:
+        print("Not a valid octal code!")
+        return
+
+# Fixes recursive download issues for different Operating Systems
+def get_r_portable(sftp, remotedir, localdir, preserve_mtime=False):
+    for entry in sftp.listdir_attr(remotedir):
+        remotepath = remotedir + "/" + entry.filename
+        localpath = os.path.join(localdir, entry.filename)
+        mode = entry.st_mode
+        if S_ISDIR(mode):
+            try:
+                os.mkdir(localpath)
+            except OSError:     
+                pass
+            get_r_portable(sftp, remotepath, localpath, preserve_mtime)
+        elif S_ISREG(mode):
+            sftp.get(remotepath, localpath, preserve_mtime=preserve_mtime)
+
+def getMultipleList():
+    pathNames = input("Enter file names seperated by a space: ")
+    pathNames = pathNames.split(" ")
+    return pathNames
+
+def getMultiple(sftp, paths, dest):
+    for path in paths:
+        getFile(sftp, path, dest)
+
+def menuLoop(sftp):
     quitLoop = False
     while not quitLoop:
-        opt = input("\nWhat do you want to do?\n(login / logoff / mkdir / ls r / ls l / get / quit)\n")
+        opt = input("\nWhat do you want to do?\n(login / logoff / mkdir / ls r / ls l / get / get m / mv l / quit)\n")
         match opt:
             case "login":
                 address = input("Enter server address: ")
@@ -80,25 +134,34 @@ def menuLoop(sftp, local):
             case "mkdir":
                 dirName = input("Please enter a directory name: ")
                 makeDir(sftp, dirName)
+            case "chmod":
+                remotePath = input("Specify Remote Path: ")
+                chmod(sftp, remotePath)
             case "ls r":
                 remotePath = input("Specify Remote Path: ")
                 printRemoteDirectory(sftp, remotePath)
             case "ls l":
                 localPath = input("Specify Local Path: ")
-                local.printLocalDirectory(localPath)
+                printLocalDirectory(localPath)
             case "get":
                 path = input("Specify file path: ")
                 dest = input("Specify destination path: ")
                 getFile(sftp, path, dest)
+            case "get m":
+                path = getMultipleList()
+                dest = input("Specify destination path: ")
+                getMultiple(sftp, path, dest)
+            case "mv l":
+                localPath = input("Specify full path to local file to rename: ")
+                renameLocalFile(localPath)
             case "quit":
                 if sftp is not None: 
                     sftp.close()
                 quitLoop = True
 
 def main():
-    local = Local()
     sftp = NoneType
-    menuLoop(sftp, local)
+    menuLoop(sftp)
 
 
 if __name__ == "__main__":
